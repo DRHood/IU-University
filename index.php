6171a6cb4447f40b13a919cde2e2a5f07fc55c6b
Darryl Hood <darryl.r.hood@gmail.com>
2020-06-17 12:22:44 -0400

    added background image for banner, light edits to index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php get_header(); ?>
+
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('assets/images/banner-img.jpg') ?>)"></div>
+    <div class="page-banner__content container t-center c-white">
+        <h1>Welcome!</h1>
+        <h2>We think you&rsquo;ll love it here.</h2>
+        <h3>Why don&rsquo;t you check out the <strong>focus</strong> you&rsquo;re interested in?</h3>
+    </div>
+</div>
